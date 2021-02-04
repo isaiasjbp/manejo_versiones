347233e168e0ecc96b1f51ec7afb9b2650adf2ab
isaiasjbp <isaias.balmaceda@gmail.com>
2021-02-03 20:37:45 -0500

    version 3.1 manejo de versiones incluimos la base url al proyecto

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 $url = $_SERVER;
-$base_url = $_SERVER['HTTP_HOST'];
+$explode_index =  explode('index.php' , $_SERVER['PHP_SELF']); 
+$base_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $explode_index[0];
+echo '<pre>'; print_r($base_url); '</pre>'; die;
 echo '<pre>'; print_r($url); '</pre>'; die;
 
 
